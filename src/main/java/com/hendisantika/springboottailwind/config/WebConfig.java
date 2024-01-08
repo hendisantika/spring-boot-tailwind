c245ee97b282b28ee45965dcf9e629b18be08976
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 15:36:27 +0700

    Add WebConfig 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springboottailwind/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springboottailwind.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-tailwind
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/8/24
+ * Time: 15:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
