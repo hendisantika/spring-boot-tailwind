ce0b1a377d6896ad5c17e79d7ab9314cc77cbd8e
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 15:33:44 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/test/java/com/hendisantika/springboottailwind/SpringBootTailwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottailwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTailwindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
